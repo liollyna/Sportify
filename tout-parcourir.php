9e300ad738fba4764f617ebceeeca25a67bb2cce
arnoguyet <arnaudguyet92@gmail.com>
2024-05-29 23:49:25 +0200

    tout parcourir réglé

--- tout-parcourir.php
+++ tout-parcourir.php
@@ -1,48 +1,9 @@
 <?php
-// Identifier le nom de la base de données
 $database = "spotify2";
+$db_handle = mysqli_connect('localhost', 'root', '', $database);
 
-// Connectez-vous à votre BDD
-$db_handle = mysqli_connect('localhost', 'root', '');
 if (!$db_handle) {
-    die("Erreur de connexion : " . mysqli_connect_error());
-}
-$db_found = mysqli_select_db($db_handle, $database);
-
-// Vérifiez si la connexion à la base de données est réussie
-if (!$db_found) {
-    die("Erreur lors de la sélection de la base de données.");
-}
-
-function afficherEnregistrements($db_handle, $sql) {
-    $result = mysqli_query($db_handle, $sql);
-
-    if ($result && mysqli_num_rows($result) > 0) {
-        echo "<h2>Liste des enregistrements</h2>";
-        echo "<table border='1'>
-                <tr>
-                    <th>activites</th>
-                    <th>coachs</th>
-                    <th>disponibilites</th>
-                    <th>messages</th>
-                    <th>rendez_vous</th>
-                    <th>utilisateurs</th>
-                </tr>";
-
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>
-                    <td>".$row["activites"]."</td>
-                    <td>".$row["coachs"]."</td>
-                    <td>".$row["disponibilites"]."</td>
-                    <td>".$row["messages"]."</td>
-                    <td>".$row["rendez_vous"]."</td>
-                    <td>".$row["utilisateurs"]."</td>
-                </tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "0 résultats";
-    }
+    die("Échec de la connexion : " . mysqli_connect_error());
 }
 
 // Vérifiez l'action à effectuer
@@ -72,6 +33,36 @@ if (isset($_POST['action'])) {
             echo "Erreur lors de la récupération des informations du coach : " . mysqli_error($db_handle);
         }
     }
+}
+
+$query_all = "SELECT * FROM activites, coachs, disponibilites, messages, rendez_vous, utilisateurs";
+$result_all = mysqli_query($db_handle, $query_all);
+
+if ($result_all && mysqli_num_rows($result_all) > 0) {
+    echo "<h2>Liste des enregistrements</h2>";
+    echo "<table border='1'>
+            <tr>
+                <th>activites</th>
+                <th>coachs</th>
+                <th>disponibilites</th>
+                <th>messages</th>
+                <th>rendez_vous</th>
+                <th>utilisateurs</th>
+            </tr>";
+
+    while ($row = mysqli_fetch_assoc($result_all)) {
+        echo "<tr>
+                <td>".$row["activites"]."</td>
+                <td>".$row["coachs"]."</td>
+                <td>".$row["disponibilites"]."</td>
+                <td>".$row["messages"]."</td>
+                <td>".$row["rendez_vous"]."</td>
+                <td>".$row["utilisateurs"]."</td>
+            </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 résultats";
 }
 
 // Fermez la connexion à la base de données
