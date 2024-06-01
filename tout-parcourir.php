1df7df21f5f2e2323b935c629ed2d6b43662fcc3
arnoguyet <arnaudguyet92@gmail.com>
2024-06-01 13:31:15 +0200

    c

--- tout-parcourir.php
+++ tout-parcourir.php
@@ -117,7 +117,7 @@ if (!isset($_SESSION['utilisateur_id'])) {
                     <li><a href="tout-parcourir.php" class="occasion-button">Tout Parcourir</a></li>
                     <li><a href="recherche.html" class="occasion-button">Recherche</a></li>
                     <li><a href="rendez-vous.html" class="occasion-button">Rendez-vous</a></li>
-                    <li><a href="votre-compte.html" class="occasion-button">Votre Compte</a></li>
+                    <li><a href="votre-compte.php" class="occasion-button">Votre Compte</a></li>
                 </ul>
             </nav>
             <br><br>
